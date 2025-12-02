9c564d5c6354a4916bbf82d7e55f4d04bb54b592
DexterM0rgan-1973 <tommaso.stramaccioni@campusdavinci.edu.it>
2025-12-02 11:31:29 +0100

    Add files via upload

---
+++ funzione 2.py
@@ -0,0 +1,25 @@
+#scrivere una funzione che dati 2 numeri interi restituisca il maggiore
+
+def nMag(num1,num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+#scrivere una procedura che dato un numero intero stampi a video
+#se è pari o dispari
+    
+def nPD (num1):
+    if num1%2==0
+        print("il numero è pari")
+    else:
+        print("il numero è dispari")
+        
+uno=input("inserire il primo numero")
+uno=int(uno)
+due=input("inserire il secondo numero")
+due=int(due)
+
+ris=nMag(uno,due)
+print(ris)
+nPD(ris)
+
